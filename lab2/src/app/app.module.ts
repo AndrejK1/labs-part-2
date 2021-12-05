f9c19ed3dd211486fda56d00ebacb45334feabca
AndrejK1 <andrju3952@gmail.com>
2021-12-05 18:30:49 +0200

    lab 2 done

---
+++ lab2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { AngularRocketComponent } from './angular-rocket/angular-rocket.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    PostFormComponent,
+    AngularRocketComponent,
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lab2/src/app/post/post.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Post } from './../app.component';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+  @Input() post: Post;
+  @Output() postid: EventEmitter<number> = new EventEmitter<number>()
+  removePost() {
+  this.postid.emit(this.post.id)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
